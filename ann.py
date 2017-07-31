0e06bec3b996b7821948758f05f61d3bcc8f3d67
Lovelyn Rose <lovelynrose@yahoo.com>
2017-07-31 10:20:31 +0530

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,4 +1,5 @@
 #Create a simple neural network with 3 layers to implement the AND function
+#Objective : Understanding of creating layers
 
 #Create Sequential Model - a linear stack of layers
 from keras.models import Sequential
@@ -14,14 +15,17 @@ model=Sequential()
 from keras.layers import Dense
 model.add(Dense(2,input_dim=2))
 #compulsorily give input size when creating the first layer
-#2-refers to the number of output units
+#2-refers to the number of units in the next layer.
+#i.e.2 input ---> 2 neurons in next layer(here hidden)
 #If user_bias=False - more time is taken to get good accuracy
 
-#hidden layer has 2 neurons
+#Next hidden layer has 2 neurons.
+#i.e.2 input neurons ---> 2 hidden layer neurons ---> 2 hidden layer neurons
 #Without hidden layer more time is taken to get good accuracy
 model.add(Dense(2))
 
 #output layer has 1 neuron
+#.i.e.2 input neurons ---> 2 hidden layer neurons ---> 2 hidden layer neurons ---> 1 neuron in the output layer
 model.add(Dense(1,activation='sigmoid'))
 
 #Compile the model
